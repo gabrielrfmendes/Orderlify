e0dd016289bdaa816df7fb507124ac97d26fd492
Gabriel <gabrielrfmendes@gmail.com>
2024-05-29 06:56:17 -0300

    feat: app background

---
+++ components/AppBackground.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
+
+type AppBackgroundProps = {
+  children?: React.ReactNode;
+}
+
+function AppBackground({ children }: AppBackgroundProps) {
+  const { colors } = useTheme();
+  const containerStyle = useMemo(() => [styles.container, {
+    backgroundColor: colors.background,
+  }], [colors.background]);
+
+  return (
+    <View style={containerStyle}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default AppBackground;
