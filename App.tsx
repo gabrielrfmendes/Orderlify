3884b2b9fbb2f9c544abc15adace50601828fe66
Gabriel <gabrielrfmendes@gmail.com>
2024-05-29 05:49:05 -0300

    chore: configuração do react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PaperProvider, Text, Button } from 'react-native-paper';
+
 function HomeScreen({ navigation }) {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text>Home Screen</Text>
 			<Button
 				title="Go to Details"
+				mode="contained"
 				onPress={() => navigation.navigate('Details')}
-			/>
+			>
+				Go to Details
+			</Button>
 		</View>
 	);
 }
@@ -26,12 +31,14 @@ const Stack = createNativeStackNavigator();
 
 function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Details" component={DetailsScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<PaperProvider>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Home" component={HomeScreen} />
+					<Stack.Screen name="Details" component={DetailsScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
 	);
 }
 
