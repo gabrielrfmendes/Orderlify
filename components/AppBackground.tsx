8df5864bd890f1c2c473b834531d3d3f4d11c61e
Gabriel <gabrielrfmendes@gmail.com>
2024-05-29 07:15:29 -0300

    style: aplicação do prettier e lint no app background

--- components/AppBackground.tsx
+++ components/AppBackground.tsx
@@ -3,26 +3,28 @@ import { View, StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
 type AppBackgroundProps = {
-  children?: React.ReactNode;
-}
-
-function AppBackground({ children }: AppBackgroundProps) {
-  const { colors } = useTheme();
-  const containerStyle = useMemo(() => [styles.container, {
-    backgroundColor: colors.background,
-  }], [colors.background]);
-
-  return (
-    <View style={containerStyle}>
-      {children}
-    </View>
-  );
+	children?: React.ReactNode;
 };
 
+function AppBackground({ children }: AppBackgroundProps) {
+	const { colors } = useTheme();
+	const containerStyle = useMemo(
+		() => [
+			styles.container,
+			{
+				backgroundColor: colors.background,
+			},
+		],
+		[colors.background]
+	);
+
+	return <View style={containerStyle}>{children}</View>;
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
 
 export default AppBackground;
